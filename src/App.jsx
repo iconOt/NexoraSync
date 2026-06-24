2983b7a05feb95133fde5d5c9cd15357e5592e02
Umar Faruq Abdul Fatah <umarfaruqabdulfatah20@gmail.com>
2026-06-24 19:20:18 +0100

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import DashboardOne from "./pages/DashboardOne";
 import DashboardTwo from "./pages/DashboardTwo";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
